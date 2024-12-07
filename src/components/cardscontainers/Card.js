4601ec2c689bffe3e7ad9a6c13540e6a3d67a99e
Tara <tara.culminique@gmail.com>
2024-12-07 15:25:10 +0100

    add like btn on cards and add npm package for a select for article's tags

--- src/components/cardscontainers/Card.js
+++ src/components/cardscontainers/Card.js
@@ -1,4 +1,6 @@
 import CardsClasses from './Card.module.css'
+import emptyHeart from '../../media/emptyheart.png'
+import fullHeart from '../../media/fullheart.png'
 
 function Card(props) {
     const handleClick = () => {
@@ -6,13 +8,16 @@ function Card(props) {
         if (props.onclick) props.onclick()
     }
     return (
-        <div onClick={handleClick}  className={CardsClasses.card}>
+        <div onClick={handleClick} className={CardsClasses.card}>
             <img src={props.image} alt="Portrait" className={CardsClasses["card-image"]}/>
             <div className={CardsClasses["card-content"]}>
                 <h3 className={CardsClasses["card-title"]}>{props.name}</h3>
                 <p className={CardsClasses["card-description"]}>
                     {props.description}
                 </p>
+                <button className={CardsClasses["card-fav"]}>
+                    <img src={emptyHeart} alt={"favorite"}/>
+                </button>
             </div>
         </div>
     )
